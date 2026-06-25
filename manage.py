3dd7c3e3d4032ee171d8b383b8c91da9ebe86dfd
Kyle D McCormick <kyle@axim.org>
2026-06-25 12:20:06 -0400

    feat: better temp manage.py

--- manage.py
+++ manage.py
@@ -1,4 +1,23 @@
 #!/bin/bash
 # A passthru until we get wip_manage.py working.
 
-cd ../openedx-platform && ./manage.py "$@"
+for envvar in \
+        "DJANGO_SETTINGS_MODULE" \
+        "STATIC_ROOT_LMS" \
+        "STATIC_ROOT_CMS" \
+        "LMS_CFG" \
+        "CMS_CFG" ; do
+    echo "$envvar=${!envvar}"
+done
+
+case "$DJANGO_SETTINGS_MODULE" in
+    *lms*) system="lms" ;;
+    *cms*) system="cms" ;;
+    *)
+        echo "could not determine system for settings $DJANGO_SETTINGS_MODULE"
+        exit 1
+        ;;
+esac
+
+set -x
+cd ../openedx-platform && ./manage.py "$system" "$@"
