32858ffbca686c404563cc9761b37a027f98deb5
Kyle D McCormick <kyle@axim.org>
2026-07-13 11:37:27 -0400

    frontend-base

---
+++ site.config.dev.tsx
@@ -0,0 +1,57 @@
+import { EnvironmentTypes, SiteConfig, footerApp, headerApp, shellApp } from '@openedx/frontend-base';
+import { authnApp } from '@openedx/frontend-app-authn';
+import { instructorDashboardApp } from '@openedx/frontend-app-instructor-dashboard';
+import { learnerDashboardApp } from '@openedx/frontend-app-learner-dashboard';
+import { notificationsApp } from '@openedx/frontend-app-notifications';
+
+import '@openedx/frontend-base/shell/style';
+import '@edx/brand/core.min.css';
+import '@edx/brand/light.min.css';
+
+const siteConfig: SiteConfig = {
+  siteId: 'frontend-template-dev',
+  siteName: 'Frontend Template Dev',
+  baseUrl: 'http://apps.local.openedx.io:8080',
+  lmsBaseUrl: 'http://local.openedx.io:8000',
+  cmsBaseUrl: 'http://studio.local.openedx.io:8001',
+  loginUrl: 'http://local.openedx.io:8000/login',
+  logoutUrl: 'http://local.openedx.io:8000/logout',
+  commonAppConfig: {
+    SUPPORT_URL: 'https://example.com/help/',
+  },
+
+  environment: EnvironmentTypes.DEVELOPMENT,
+  apps: [
+    shellApp,
+    headerApp,
+    footerApp,
+    authnApp,
+    learnerDashboardApp,
+    {
+      ...instructorDashboardApp,
+      config: {
+        ...instructorDashboardApp.config,
+        SUPPORT_URL: 'https://example.com/help/instructor',
+      },
+    },
+    notificationsApp,
+  ],
+  externalRoutes: [
+    {
+      role: 'org.openedx.frontend.role.profile',
+      url: 'http://apps.local.openedx.io:1995/profile/'
+    },
+    {
+      role: 'org.openedx.frontend.role.account',
+      url: 'http://apps.local.openedx.io:1997/account/'
+    },
+    {
+      role: 'org.openedx.frontend.role.logout',
+      url: 'http://local.openedx.io:8000/logout'
+    },
+  ],
+
+  accessTokenCookieName: 'edx-jwt-cookie-header-payload',
+};
+
+export default siteConfig;
